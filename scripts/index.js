0868ac208fdcd400a0478acbe61a9f2720b85985
carlangasmotta17 <carlossmotaa.21@gmail.com>
2024-02-28 11:18:33 -0300

    segundo commit

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+// script.js
+
+// Clase Activity
+class Activity {
+    constructor(id, title, description, imgUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+}
+
+
+class Repository {
+    constructor() {
+        this.activities = [];
+    }
+
+    getAllActivities() {
+        return this.activities;
+    }
+
+    createActivity(id, title, description, imgUrl) {
+        const activity = new Activity(id, title, description, imgUrl);
+        this.activities.push(activity);
+    }
+
+
+    deleteActivity(id) {
+        this.activities = this.activities.filter(activity => activity.id !== id);
+    }
+}
+
+
+const activityRepository = new Repository();
